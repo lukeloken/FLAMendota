a5230b017190cf47a88a9d5239a632982a2c7a4c
lukeloken <lukeloken@gmail.com>
2016-10-20 13:46:28 -0500

    Workflow to download and plot Buoy DAta

---
+++ R/BuoyWorkFlow.R
@@ -0,0 +1,18 @@
+# Code to plot Timeseries Temperature Depth Data
+
+
+source("R/ExtractBuoyData.R") #GetBuoyData
+source("R/PlotBuoyTemp.R") # TempHeatTS
+
+#What data do you want (all characters)
+startdate<-"2014-01-31"
+enddate<-"2015-12-31"
+type="Daily"
+
+#Download Buoy Data from LTER server and crop as needed
+BuoyData<-GetBuoyTemp(startdate=startdate, enddate=enddate, type=type)
+BuoyData<-BuoyData[order(BuoyData$sampledate),]
+BuoyData$wtemp[BuoyData$wtemp<0]<-NA
+
+#Plot Lake Temperature Profile Time Series
+TempHeatTS(BuoyData)
