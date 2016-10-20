9996cd979d3c1222b0d5d94647ad742b60aa863d
lukeloken <lukeloken@gmail.com>
2016-10-20 14:20:05 -0500

    Use package 'tidyr' rather than for loops to spread dataframe.

--- R/PlotBuoyTemp.R
+++ R/PlotBuoyTemp.R
@@ -1,22 +1,21 @@
 # Code to plot Timeseries Temperature Depth Data
 # Input is a dataframe consisting of "sampledate", "depth", and "wtemp" as columns. 
 
+library(tidyr)
+library(gtools)
 
 TempHeatTS<-function (LTERdf){
   
   depths = unique(LTERdf$depth)
   dates = seq(min(LTERdf$sampledate), max(LTERdf$sampledate), by='days')
+  dates.df<-data.frame(sampledate = c(dates))
   
   #Make Matrix of temperature (x=depth, y=date, z=temp)
-  wrt<-matrix(ncol=length(depths), nrow=length(dates))
-  for (i in 1:length(dates)){
-    for (j in 1:length(depths)){
-      if (length(LTERdf$wtemp[LTERdf$depth==depths[j] & LTERdf$sampledate==dates[i]])==1){
-        wrt[i,j]<-LTERdf$wtemp[LTERdf$depth==depths[j] & LTERdf$sampledate==dates[i]]
-        }
-    }
-  }
+  wrt1<-spread(LTERdf[c('sampledate', 'depth', 'wtemp')], key=depth, value=wtemp, fill=NA )
+  wrt2<-merge(wrt1, dates.df, by='sampledate', all=T)
+  wrt<-as.matrix(wrt2[,-1])
   
+  #Plot
   filled.contour(x=dates, y=depths, z=wrt, ylim=c(max(depths), 0), nlevels = 100, color.palette = colorRampPalette(c("violet", "blue", "cyan", "green3", "yellow", "orange", "red"), bias = 1, space = "rgb"), ylab="Depths (m)")
   
 }
