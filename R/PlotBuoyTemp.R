db5e4a1e9b59fabb7c66ae61a870e7c2d43fbaa4
lukeloken <lukeloken@gmail.com>
2016-10-20 13:36:17 -0500

    Turned plot heat map into separate function

--- R/PlotBuoyTemp.R
+++ R/PlotBuoyTemp.R
@@ -1,36 +1,20 @@
 # Code to plot Timeseries Temperature Depth Data
+# Input is a dataframe consisting of "sampledate", "depth", and "wtemp" as columns. 
 
-
-source("R/ExtractBuoyData.R")
-
-startdate<-"2015-01-31"
-enddate<-"2015-12-31"
-type="Daily"
-
-
-BuoyData<-GetBuoyTemp(startdate=startdate, enddate=enddate, type=type)
-BuoyData<-BuoyData[order(BuoyData$sampledate),]
-BuoyData$wtemp[BuoyData$wtemp<0]<-NA
-
-# LTERdf<-BuoyData
-
-source("R/PlotBuoyTemp.R")
 
 TempHeatTS<-function (LTERdf){
   
-
   depths = unique(LTERdf$depth)
   dates = unique(LTERdf$sampledate)
   
+  #Make Matrix of temperature (x=depth, y=date, z=temp)
   wrt<-matrix(ncol=length(depths), nrow=length(dates))
-  
   for (i in 1:length(dates)){
     for (j in 1:length(depths)){
       wrt[i,j]<-LTERdf$wtemp[LTERdf$depth==depths[j] & LTERdf$sampledate==dates[i]]
     }
   }
   
-  # n = length(dates)
   filled.contour(x=dates, y=depths, z=wrt, ylim=c(max(depths), 0), nlevels = 100, color.palette = colorRampPalette(c("violet", "blue", "cyan", "green3", "yellow", "orange", "red"), bias = 1, space = "rgb"), ylab="Depths (m)")
   
 }
