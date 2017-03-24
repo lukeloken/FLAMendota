e4e59b3c6c4869bad6a6c30777e3cb7f133bf394
lukeloken <lukeloken@gmail.com>
2017-03-24 15:03:04 -0500

    Plot Lake Mendota 2016 Temperature Bathygram

--- R/BuoyWorkFlow.R
+++ R/BuoyWorkFlow.R
@@ -1,19 +1,20 @@
 # Code to plot Timeseries Temperature Depth Data
 
-
 source("R/ExtractBuoyData.R") #GetBuoyData
 source("R/PlotBuoyTemp.R") # TempHeatTS
 
-#What data do you want (all characters)
-startdate<-"2016-01-01"
-enddate<-"2016-12-31"
-type="Daily"
+# What data do you want (all characters)
+# This is for downloading buoy data from lter data portal
+# startdate<-"2016-01-01"
+# enddate<-"2016-12-31"
+# type="Daily"
 
 # #Download Buoy Data from LTER server and crop as needed
 # BuoyData<-GetBuoyTemp(startdate=startdate, enddate=enddate, type=type)
 # BuoyData<-BuoyData[order(BuoyData$sampledate),]
 # BuoyData$wtemp[BuoyData$wtemp<0]<-NA
 
+# Load 2016 Data
 BuoyData2016<-read.csv(file='Data/MendotaDaily2016.csv', header=T, stringsAsFactors = F)
 BuoyData2016$sampledate<-as.Date(BuoyData2016$sampledate)
 BuoyData2016<-BuoyData2016[BuoyData2016$year4==2016,]
@@ -21,7 +22,21 @@ head(BuoyData2016)
 str(BuoyData2016)
 
 
-
 #Plot Lake Temperature Profile Time Series
-TempHeatTS(BuoyData)
-TempHeatTS(BuoyData2016)
+# TempHeatTS(BuoyData)
+
+png('Figures/TempBathyGram2016.png', width=8, height=4, units='in', res=200, bg='white')
+par(pch=16)
+par(ps=12)
+par(mfrow=c(1,1))
+par(mar = c(3,3.5,0.5,0.5),mgp=c(1.5,0.4,0),tck=-0.02)
+par(lend=2)
+
+xticks<-seq(ceiling_date(min(BuoyData2016$sampledate), "months"),floor_date(max(BuoyData2016$sampledate), "months"), by='months')
+xlabels<-paste(month(xticks, label=TRUE, abbr=T), " 1", sep="")
+
+TempHeatTS(BuoyData2016, xticks, xlabels)
+mtext(expression(paste("Temperature (", degree, "C)", sep="")), 4, -6)
+
+dev.off()
+
