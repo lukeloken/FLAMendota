05c895680bcf578f981a0e1c9f9d098ea9371a93
lukeloken <lukeloken@gmail.com>
2017-03-24 14:47:38 -0500

    Uploaded 2016 Buoy Data and plot

--- R/BuoyWorkFlow.R
+++ R/BuoyWorkFlow.R
@@ -5,14 +5,23 @@ source("R/ExtractBuoyData.R") #GetBuoyData
 source("R/PlotBuoyTemp.R") # TempHeatTS
 
 #What data do you want (all characters)
-startdate<-"2012-01-31"
-enddate<-"2015-12-31"
+startdate<-"2016-01-01"
+enddate<-"2016-12-31"
 type="Daily"
 
-#Download Buoy Data from LTER server and crop as needed
-BuoyData<-GetBuoyTemp(startdate=startdate, enddate=enddate, type=type)
-BuoyData<-BuoyData[order(BuoyData$sampledate),]
-BuoyData$wtemp[BuoyData$wtemp<0]<-NA
+# #Download Buoy Data from LTER server and crop as needed
+# BuoyData<-GetBuoyTemp(startdate=startdate, enddate=enddate, type=type)
+# BuoyData<-BuoyData[order(BuoyData$sampledate),]
+# BuoyData$wtemp[BuoyData$wtemp<0]<-NA
+
+BuoyData2016<-read.csv(file='Data/MendotaDaily2016.csv', header=T, stringsAsFactors = F)
+BuoyData2016$sampledate<-as.Date(BuoyData2016$sampledate)
+BuoyData2016<-BuoyData2016[BuoyData2016$year4==2016,]
+head(BuoyData2016)
+str(BuoyData2016)
+
+
 
 #Plot Lake Temperature Profile Time Series
 TempHeatTS(BuoyData)
+TempHeatTS(BuoyData2016)
