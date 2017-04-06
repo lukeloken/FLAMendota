b2c4179133f4a3c8395ce146d0315748acb06700
lukeloken <lukeloken@gmail.com>
2017-04-06 10:21:40 -0500

    create function to calculate flux

--- R/CalculateFlux.R
+++ R/CalculateFlux.R
@@ -1,8 +1,35 @@
 
+library(LakeMetabolizer)
+
+#Test names
+spdf<-Mendota_surface_UTM
+temp<-mean(spdf$TempC, na.rm=T)
+conc_name<-'CH4uM_t'
+K600_name<-'k600'
+gas<-'CH4'
 
 #Convert k600 to k for CO2 at temperature of system. 
 
-library(LakeMetabolizer)
+SpatailFlux<-function(spdf, conc_name, temp, K600_name, gas){
+
+  # conc is in micromol per liter
+  conc<-spdf@data[,c(conc_name)] 
+  
+  # kgas is in cm/hr 
+  kgas<-k600.2.kGAS.base(spdf@data[,c(K600_name)], temp, gas)
+  
+  # conc.sat is in micromol per liter
+  conc.sat<- 0.003 # Need to figure this out (based on temperature, pressure)
+  
+  # flux_out is in mmol per meter squared per day
+  # Equations includes conversions: cm/hr to m/d ; micromol/liter to milimol/meter cubed
+  flux_out<-(conc- conc.sat)*kgas*24/100
+
+  return(flux_out)
+  
+}
+
+  
 
 # wind.scale(wind, height, units) # Function to convert wind speed to U10. 
 # wind.scale(wind data frame..., height, units)
