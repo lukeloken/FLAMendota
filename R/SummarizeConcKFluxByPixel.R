b7034edb979f80b6809eed4ea05c8ad874bef51b
lukeloken <lukeloken@gmail.com>
2017-05-26 14:38:01 -0500

    New script to summarize Lake Mendota spatial data by pixel. Inputs include interpolated flame data (co2, ch4, etc), modeled k data (based on wind), and flux data (based on k and conc)

---
+++ R/SummarizeConcKFluxByPixel.R
@@ -0,0 +1,21 @@
+
+# ################################################################################
+# Code to summarize 2016 Daily Concentration, k, and flux by pixel
+# input includes daily maps of each variable as lists
+# output is map that includes information on how each pixel behaves through time. 
+# ################################################################################
+
+# packages
+
+# source data
+
+# conc
+conc.list<-
+  
+# k
+k.list<-
+  
+# flux
+flux.list<-
+  
+  
