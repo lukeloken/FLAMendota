78594ea746b12076b4c8442bcebba3f68cd12e91
lukeloken <lukeloken@gmail.com>
2017-05-30 11:15:47 -0500

    Code to merge all Flame interpolated maps. This code loops through flame directory and downloads each idw map and saves it to a common list.

---
+++ R/CoalesceInterpolatedFlameMaps.R
@@ -0,0 +1,36 @@
+# =============================================
+# Code to Extract Flame Data from Loken Desktop
+# Bring all interpolated maps into a single R object (list)
+# Inputs = individual idw surface
+# outputs = list of surfaces
+# =============================================
+
+# Load packages
+library(gtools)
+
+#### Merge all spatial summaries ####
+
+data_dir<-"E:/Dropbox/FLAME_YaharaLakes/Data"
+
+dates<-list.files(data_dir)
+dates2016<-dates[grep('2016', dates)]
+dates2016<-dates2016[grep('LakeMendota', dates2016)]
+
+maps_list<-list()
+
+dir<-1
+for (dir in 1:length(dates2016)){
+  file_dir<-paste(data_dir, '/', dates2016[dir], '/shapefiles_idw', sep="")
+  files<-list.files(file_dir)
+  filename<-files[grep('cleaned.dbf', files)]
+  loadname<-sub('.dbf', '', filename)
+  
+  shapefile<-readOGR(file_dir, loadname)
+  maps_list[[dir]]<-shapefile
+  names(maps_list)[[dir]]<-loadname
+}
+
+# Save to Git folder
+saveRDS(maps_list, file='Data/All2016Maps.rds')
+
+
