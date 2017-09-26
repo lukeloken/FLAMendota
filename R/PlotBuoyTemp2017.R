78f80499790727bb5eb67b9cdc5fa346aa0230ab
lukeloken <lukeloken@gmail.com>
2017-09-26 14:28:34 -0500

    added functions to calculate metalimnion depth and included wind speed on output figure

--- R/PlotBuoyTemp2017.R
+++ R/PlotBuoyTemp2017.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(gtools)
 library(LakeMetabolizer)
 library(rLakeAnalyzer)
+library(stringr)
 
 # LTERdf<-read.csv('Data/sensor_mendota_lake_watertemp_hourly2017.csv', header=T, stringsAsFactors = F)
 # LTERdf$sample_time<-str_pad(LTERdf$hour, 4, pad = "0")
@@ -15,6 +16,7 @@ LTERdf$sample_time<-str_pad(LTERdf$sample_time, 4, pad = "0")
 LTERdf$datetime<-as.POSIXct(paste(LTERdf$sampledate, LTERdf$sample_time, sep=" "), format='%Y-%m-%d %H%M')
 LTERdf_subset<-LTERdf[LTERdf$datetime>as.POSIXct('2017-09-07 00:00:00'),]
 
+LTERdf_subset<-LTERdf_subset[order(LTERdf_subset$datetime),]
 
 depths = unique(LTERdf_subset$depth)
 dates = seq(min(LTERdf_subset$datetime), max(LTERdf_subset$datetime), by='mins')
@@ -26,8 +28,15 @@ wrt1<-spread(LTERdf_subset[c('datetime', 'depth', 'wtemp')], key=depth, value=wt
 wrt2<-merge(wrt1, dates.df, by='datetime', all=T)
 wrt<-as.matrix(wrt2[,-1])
 
+m.d.1 <- apply(wrt1[,-1], 1, function (x) meta.depths(x, depths=depths)[1])
+m.d.2 <- apply(wrt1[,-1], 1, function (x) meta.depths(x, depths=depths)[2])
 
-png('Figures/TempBathyGram2017.png', width=7.15, height=3.5, units='in', res=200, bg='white')
+plot(wrt1[,1],m.d.1, ylim=c(20,0), type='l')
+points(wrt1[,1], m.d.2, col='red', type='l')
+
+polygon(x=c(wrt1[,1], rev(wrt1[,1])), y=c(m.d.2, rev(m.d.1)), col='grey')
+
+png('Figures/TempBathyGram2017.png', width=7.15, height=3.5, units='in', res=400, bg='white')
 par(pch=16)
 par(ps=10)
 par(mfrow=c(1,1))
@@ -49,18 +58,27 @@ LTERmet_subset<-LTERmet[LTERmet$datetime>as.POSIXct('2017-09-07 00:00:00'),]
 
 
 
-png('Figures/AitTemp2017.png', width=7.15, height=3.5, units='in', res=200, bg='white')
+png('Figures/AitTemp2017.png', width=7.15, height=3.5, units='in', res=600, bg='white')
 par(pch=16)
 par(ps=10)
-par(mfrow=c(1,1))
-par(mar = c(3,3.5,0.5,7.5),mgp=c(1.5,0.4,0),tck=-0.02)
+par(mfrow=c(2,1))
+par(mar = c(0,3.5,0.5,7.5),mgp=c(1.5,0.4,0), oma=c(1.5,0,0,0), tck=-0.02)
 par(lend=2)
+colors<-c('darkred', 'darkblue')
 
-plot(LTERmet_subset$datetime, LTERmet_subset$avg_air_temp, type='l', xlab='', ylab='', las=1, xaxs='i')
-mtext(expression(paste('Air temperature (', degree, 'C)')), 2, 2)
+plot(LTERmet_subset$datetime, LTERmet_subset$avg_air_temp, type='l', xlab='', ylab='', las=1, xaxs='i', yaxt='n', col=colors[1], lwd=2, xaxt='n')
+axis(1, labels=NA)
+axis(4, las=1, col=colors[1], col.ticks=colors[1], col.axis=colors[1])
+mtext(expression(paste('Air temperature (', degree, 'C)')), 4, 1.5, col=colors[1])
 par(new=T)
 plot(LTERmet_subset$datetime, LTERmet_subset$avg_air_temp*9/5+32, type='n', xlab='', ylab='', axes=F, xaxs='i')
-axis(4, las=1)
-mtext(expression(paste('Air temperature (', degree, 'F)')), 4, 2)
+axis(4, las=1, line=3, col=colors[1], col.axis=colors[1])
+mtext(expression(paste('(', degree, 'F)')), 4, 4.5, col=colors[1])
+
+# par(new=T)
+plot(LTERmet_subset$datetime, LTERmet_subset$avg_wind_speed, type='l', xlab='', ylab='', las=1, xaxs='i', col=colors[2], yaxt='n', lwd=2)
+mtext(expression(paste('Wind speed (m s'^'-1', ')')), 4, 1.5, col=colors[2])
+axis(4, las=1, col=colors[2], col.ticks=colors[2], col.axis=colors[2])
 
 dev.off()
+
