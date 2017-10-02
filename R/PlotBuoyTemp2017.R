69dde4110164daebd4eda0c804b1e6c3de6c97e8
lukeloken <lukeloken@gmail.com>
2017-10-02 08:59:02 -0500

    updated to week of Oct 2

--- R/PlotBuoyTemp2017.R
+++ R/PlotBuoyTemp2017.R
@@ -17,7 +17,7 @@ colors<-c('darkred', 'darkblue', 'olivedrab')
 
 # LTERdf<-read.csv('Data/sensor_mendota_lake_watertemp_hi_res_Sep2017.csv', header=T, stringsAsFactors = F)
 
-LTERdf<-read.csv('Data/sensor_mendota_lake_watertemp_hi_res_Sep28_2017.csv', header=T, stringsAsFactors = F)
+LTERdf<-read.csv('Data/sensor_mendota_lake_watertemp_hi_res_Oct2_2017.csv', header=T, stringsAsFactors = F)
 LTERdf$sample_time<-str_pad(LTERdf$sample_time, 4, pad = "0")
 
 LTERdf$datetime<-as.POSIXct(paste(LTERdf$sampledate, LTERdf$sample_time, sep=" "), format='%Y-%m-%d %H%M')
@@ -118,7 +118,7 @@ mtext(expression(paste("Water temperature (", degree, "C)", sep="")), 4, -6)
 dev.off()
 
 
-LTERmet<-read.csv('Data/sensor_mendota_lake_met_hourly_Sep28_2017.csv', header=T, stringsAsFactors = F)
+LTERmet<-read.csv('Data/sensor_mendota_lake_met_hourly_Oct2_2017.csv', header=T, stringsAsFactors = F)
 LTERmet$sample_time<-str_pad(LTERmet$hour, 4, pad = "0")
 
 LTERmet$datetime<-as.POSIXct(paste(LTERmet$sampledate, LTERmet$sample_time, sep=" "), format='%Y-%m-%d %H%M')
@@ -135,7 +135,7 @@ par(lend=2)
 
 plot(LTERmet_subset$datetime, LTERmet_subset$avg_air_temp, type='l', xlab='', ylab='', las=1, xaxs='i', yaxt='n', col=colors[1], lwd=2, xaxt='n')
 axis(1, labels=NA, at=xticks)
-axis(4, las=1, col=colors[1], col.ticks=colors[1], col.axis=colors[1])
+axis(4, las=1,  col.ticks=colors[1], col.axis=colors[1])
 mtext(expression(paste('Air temperature (', degree, 'C)')), 4, 1.5, col=colors[1])
 par(new=T)
 plot(LTERmet_subset$datetime, LTERmet_subset$avg_air_temp*9/5+32, type='n', xlab='', ylab='', axes=F, xaxs='i')
@@ -146,7 +146,7 @@ mtext(expression(paste('(', degree, 'F)')), 4, 4.5, col=colors[1])
 plot(LTERmet_subset$datetime, LTERmet_subset$avg_wind_speed, type='l', xlab='', ylab='', las=1, xaxs='i', col=colors[2], yaxt='n', lwd=2, xaxt='n')
 axis(1, labels=xlabels, at=xticks)
 mtext(expression(paste('Wind speed (m s'^'-1', ')')), 4, 1.5, col=colors[2])
-axis(4, las=1, col=colors[2], col.ticks=colors[2], col.axis=colors[2])
+axis(4, las=1, col.ticks=colors[2], col.axis=colors[2])
 
 dev.off()
 
