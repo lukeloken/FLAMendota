688e56726e0a85afa2847d401ec294acaab87d35
lukeloken <lukeloken@gmail.com>
2017-10-25 13:43:50 -0500

    calcualte CV and pecent diff thorugh time

--- R/CalculateConcFluxVariation.R
+++ R/CalculateConcFluxVariation.R
@@ -1,15 +1,16 @@
 
-
+source('R/CalculateGasSaturation.R')
+colors<-c('darkblue', 'darkred')
 
 xticks<-seq(ceiling_date(min(LGRList$Mean$Date), "months"),floor_date(max(LGRList$Mean$Date), "months"), by='months')
 xlabels<-paste(month(xticks, label=TRUE, abbr=T), " 1", sep="")
 buoypch<-20
 
 
-plot(LGRList$Mean$Date, (LGRList$Q75$CH4uM_t-LGRList$Q25$CH4uM_t)/(LGRList$Q75$CH4uM_t+LGRList$Q25$CH4uM_t), type="b", pch=15, ylab="", xlab="", xaxt="n", col="red", ylim=c(0,.4))
+plot(LGRList$Mean$Date, (LGRList$Q75$CH4uM_t-LGRList$Q25$CH4uM_t)/(LGRList$Q75$CH4uM_t+LGRList$Q25$CH4uM_t), type="b", pch=15, ylab="", xlab="", xaxt="n", col=colors[2], ylim=c(0,.4))
 
 
-points(LGRList$Mean$Date, (LGRList$Q75$CO2uM_t-LGRList$Q25$CO2uM_t)/(LGRList$Q75$CO2uM_t+LGRList$Q25$CO2uM_t), type="b", pch=15, ylab="", xlab="", xaxt="n", col="blue")
+points(LGRList$Mean$Date, (LGRList$Q75$CO2uM_t-LGRList$Q25$CO2uM_t)/(LGRList$Q75$CO2uM_t+LGRList$Q25$CO2uM_t), type="b", pch=15, ylab="", xlab="", xaxt="n", col=colors[1])
 
 axis(1, at=xticks, labels=xlabels)
 
@@ -17,14 +18,23 @@ mtext('Quartile coefficient of dispersion', 2, 2)
 
 
 
-plot(LGRList$Mean$Date, (LGRList$sd$CH4uM_t/LGRList$Mean$CH4uM_t), type="b", pch=15, ylab="", xlab="", xaxt="n", col="red", ylim=c(0,1.8))
+plot(LGRList$Mean$Date, (LGRList$sd$CH4uM_t/LGRList$Mean$CH4uM_t), type="b", pch=15, ylab="", xlab="", xaxt="n", col=colors[2], ylim=c(0,1.8))
 
-points(LGRList$Mean$Date, (LGRList$sd$CO2uM_t/LGRList$Mean$CO2uM_t), type="b", pch=15, ylab="", xlab="", xaxt="n", col="blue")
+points(LGRList$Mean$Date, (LGRList$sd$CO2uM_t/LGRList$Mean$CO2uM_t), type="b", pch=15, ylab="", xlab="", xaxt="n", col=colors[1])
 
 axis(1, at=xticks, labels=xlabels)
 
 
 mtext('Coefficient of variation', 2, 2)
+
+
+
+plot(LGRList$Mean$CH4uM_t~(LGRList$sd$CH4uM_t/LGRList$Mean$CH4uM_t), type="p", pch=15, ylab="mean CH4", xlab="CV CH4", col=colors[2], ylim=c(0,1.8))
+
+plot(LGRList$Mean$CO2uM_t~(LGRList$sd$CO2uM_t/LGRList$Mean$CO2uM_t), type="p", pch=15, ylab="mean CO2", xlab="CV CO2",  col=colors[1])
+
+axis(1, at=xticks, labels=xlabels)
+
 
 ConcArray<-readRDS(file='Data/DailyFlamebyPixel.rds')
 
@@ -32,13 +42,6 @@ fluxmatrix<-readRDS('Data/DailyFluxperPixel.rds')
 str(fluxmatrix)
 sd(fluxmatrix)
 
-# Kmatrix<-readRDS('Data/Kmatrix.rds')
-# 
-# plot(ConcArray[,,'CO2uM_t'], fluxmatrix[,,'CO2'])
-# plot(ConcArray[,,'CH4uM_t'], fluxmatrix[,,'CH4'])
-# 
-# plot(Kmatrix[,], fluxmatrix[,,'CO2'])
-# plot(Kmatrix[,], fluxmatrix[,,'CH4'])
 
 
 
@@ -53,6 +56,10 @@ Q5CO2<-apply(ConcArray[,,'CO2uM_t'],1,quantile, probs=0.05)
 Q95CO2<-apply(ConcArray[,,'CO2uM_t'],1,quantile, probs=0.95)
 mean(Q95CO2-Q5CO2)
 
+Q25CO2<-apply(ConcArray[,,'CO2uM_t'],1,quantile, probs=0.25)
+Q75CO2<-apply(ConcArray[,,'CO2uM_t'],1,quantile, probs=0.75)
+mean(Q75CO2-Q25CO2)
+
 sdCH4conc<-apply(ConcArray[,,'CH4uM_t'],1,sd) 
 meanCH4conc<-apply(ConcArray[,,'CH4uM_t'],1,mean)
 minCH4conc<-apply(ConcArray[,,'CH4uM_t'],1,min)
@@ -63,8 +70,10 @@ Q5CH4<-apply(ConcArray[,,'CH4uM_t'],1,quantile, probs=0.05)
 Q95CH4<-apply(ConcArray[,,'CH4uM_t'],1,quantile, probs=0.95)
 mean(Q95CH4-Q5CH4)
 
-gas.at.sat(c(5.793, 27.22), gas='CH4', atm_conc=median(Atm$XCH4Dppm))
-gas.at.sat(c(5.793, 27.22), gas='CO2', atm_conc=median(Atm$XCO2Dppm))
+Q25CH4<-apply(ConcArray[,,'CH4uM_t'],1,quantile, probs=0.25)
+Q75CH4<-apply(ConcArray[,,'CH4uM_t'],1,quantile, probs=0.75)
+mean(Q75CH4-Q25CH4)
+
 
 
 sdCO2flux<-apply(fluxmatrix[,,'CO2'],1,sd) 
@@ -77,6 +86,10 @@ MADCO2flux<-apply(fluxmatrix[,,'CO2'],1,mad)
 QuantileDispersionCO2flux<-(Q3CO2flux-Q1CO2flux)/(Q3CO2flux+Q1CO2flux)
 sumCO2flux<-apply(fluxmatrix[,,'CO2'],1,sum)
 
+Q5CO2flux<-apply(fluxmatrix[,,'CO2'],1,quantile, probs=0.05)
+Q95CO2flux<-apply(fluxmatrix[,,'CO2'],1,quantile, probs=0.95)
+mean(Q95CO2flux-Q5CO2flux)
+
 summary(data.frame(minCO2flux, meanCO2flux, maxCO2flux))
 
 sdCH4flux<-apply(fluxmatrix[,,'CH4'],1,sd) 
@@ -84,6 +97,10 @@ meanCH4flux<-apply(fluxmatrix[,,'CH4'],1,mean)
 minCH4flux<-apply(fluxmatrix[,,'CH4'],1,min)
 maxCH4flux<-apply(fluxmatrix[,,'CH4'],1,max)
 sumCH4flux<-apply(fluxmatrix[,,'CH4'],1,sum)
+
+Q5CH4flux<-apply(fluxmatrix[,,'CH4'],1,quantile, probs=0.05)
+Q95CH4flux<-apply(fluxmatrix[,,'CH4'],1,quantile, probs=0.95)
+mean(Q95CH4flux-Q5CH4flux)
 
 
 summary(data.frame(minCH4flux, meanCH4flux, maxCH4flux))
@@ -131,4 +148,215 @@ meanCH4Sat<-apply(ConcArray[,,c('CH4St_t')],1,mean)
 maxCH4Sat<-apply(ConcArray[,,c('CH4St_t')],1,max) 
 minCH4Sat<-apply(ConcArray[,,c('CH4St_t')],1,min) 
 summary(data.frame(minCH4Sat, meanCH4Sat, maxCH4Sat))
+
+
+
+#Conc percent diff
+CH4ConcPerDiff<-100*abs((BuoyFlux$CH4Buoy_Conc_Mean-LakeFlux$CH4_Conc_Mean)/rowMeans(data.frame(BuoyFlux$CH4Buoy_Conc_Mean,LakeFlux$CH4_Conc_Mean)))
+
+CO2ConcPerDiff<-100*abs((BuoyFlux$CO2Buoy_Conc_Mean-LakeFlux$CO2_Conc_Mean)/rowMeans(data.frame(BuoyFlux$CO2Buoy_Conc_Mean,LakeFlux$CO2_Conc_Mean)))
+
+
+summary(CO2ConcPerDiff)
+summary(CH4ConcPerDiff)
+
+summary(BuoyFlux$CO2Buoy_Conc_Mean)
+summary(BuoyFlux$CH4Buoy_Conc_Mean)
+
+summary(LakeFlux$CO2_Conc_Mean)
+summary(LakeFlux$CH4_Conc_Mean)
+
+
+
+
+#Flux
+CH4PerDiff<-100*abs((BuoyFlux$CH4Buoy_Flux_Mean-LakeFlux$CH4_Flux_Mean)/rowMeans(data.frame(BuoyFlux$CH4Buoy_Flux_Mean,LakeFlux$CH4_Flux_Mean)))
+
+CO2PerDiff<-100*abs((BuoyFlux$CO2Buoy_Flux_Mean-LakeFlux$CO2_Flux_Mean)/rowMeans(data.frame(BuoyFlux$CO2Buoy_Flux_Mean,LakeFlux$CO2_Flux_Mean)))
+
+summary(CO2PerDiff)
+summary(CH4PerDiff)
+
+summary(BuoyFlux$CO2Buoy_Flux_Mean)
+summary(BuoyFlux$CH4Buoy_Flux_Mean)
+
+summary(LakeFlux$CO2_Flux_Mean)
+summary(LakeFlux$CH4_Flux_Mean)
+
+#Percent Error
+#Conc
+CH4ConcPerError<-100*abs((BuoyFlux$CH4Buoy_Conc_Mean-LakeFlux$CH4_Conc_Mean)/LakeFlux$CH4_Conc_Mean)
+
+CO2ConcPerError<-100*abs((BuoyFlux$CO2Buoy_Conc_Mean-LakeFlux$CO2_Conc_Mean)/LakeFlux$CO2_Conc_Mean)
+
+summary(CH4ConcPerError)
+summary(CO2ConcPerError)
+
+#Flux
+CH4PerError<-100*abs((BuoyFlux$CH4Buoy_Flux_Mean-LakeFlux$CH4_Flux_Mean)/LakeFlux$CH4_Flux_Mean)
+
+CO2PerError<-100*abs((BuoyFlux$CO2Buoy_Flux_Mean-LakeFlux$CO2_Flux_Mean)/LakeFlux$CO2_Flux_Mean)
+
+summary(CO2PerError)
+summary(CH4PerError)
+
+#PercentDiffThroughTime
+png('Figures/PercentDiffTS.png', width=3, height=5, units='in', res=200, bg='white')
+par(ps=12)
+par(mgp=c(2.5,0.4,0),tck=-0.02)
+par(lend=1)
+par(lwd=1)
+lty=c(1,2)
+pt.cex=.8
+alpha=0.6
+# colors<-c('darkblue', 'darkred')
+
+par(mfcol=c(4,1))
+par(mar=c(2,4,0.5,0.5))
+par(pch=1)
+ylim=c(0,100)
+
+plot(BuoyFlux$date_names, CO2ConcPerDiff, ylim=ylim, col=colors[1], xlab='', ylab='', cex=pt.cex)
+abline(h=0)
+abline(h=median(CO2ConcPerDiff), col=colors[1], lty=2)
+legend('top', expression(paste(CO[2], ' concentration', sep='')), bty='n')
+
+plot(BuoyFlux$date_names, CH4ConcPerDiff, ylim=ylim, col=colors[2], xlab='', ylab='', cex=pt.cex)
+abline(h=0)
+abline(h=median(CH4ConcPerDiff), col=colors[2], lty=2)
+legend('top', expression(paste(CH[4], ' concentration', sep='')), bty='n')
+
+
+plot(BuoyFlux$date_names, CO2PerDiff, ylim=ylim, col=colors[1], xlab='', ylab='', cex=pt.cex)
+abline(h=0)
+abline(h=median(CO2PerDiff), col=colors[1], lty=2)
+legend('top', expression(paste(CO[2], ' flux', sep='')), bty='n')
+
+
+plot(BuoyFlux$date_names, CH4PerDiff, ylim=ylim, col=colors[2], xlab='', ylab='', cex=pt.cex)
+abline(h=0)
+abline(h=median(CH4PerDiff), col=colors[2], lty=2)
+legend('top', expression(paste(CH[4], ' flux', sep='')), bty='n')
+
+
+mtext('Percent Differnece (%)', 2, -1.5, outer=T)
+
+dev.off()
+
+#PercentDiffHistorgram
+png('Figures/PercentDiffHist.png', width=4, height=4, units='in', res=200, bg='white')
+par(ps=10)
+par(mgp=c(2.5,0.4,0),tck=-0.02)
+par(lend=1)
+par(lwd=1)
+pt.cex=.8
+alpha=0.6
+# colors<-c('darkblue', 'darkred')
+par(mfcol=c(2,2))
+par(mar=c(0.25,1.25,0.25,0.25), oma=c(2.5,1.5,1,0))
+par(pch=1)
+xlim=c(0,125)
+breaks<-seq(0,100000,5)
+xaxis<-seq(0,125,25)
+
+hist(CO2ConcPerDiff, col=add.alpha(colors[1], alpha), main='', xlab='', ylab='', xlim=xlim, breaks=breaks, xaxt='n', las=1)
+box(which='plot')
+axis(1, at=xaxis, labels=NA)
+mtext('Concentration', 3, 0)
+legend('topright', inset=0.01, c(expression(CO[2]), expression(CH[4])), bty='n', pch=22, pt.cex=3, pt.bg=c(add.alpha(c(colors[1:2]), alpha)), y.intersp=2)
+
+
+hist(CH4ConcPerDiff, col=add.alpha(colors[2], alpha), main='', xlab='', ylab='', xlim=xlim, breaks=breaks, xaxt='n', las=1)
+box(which='plot')
+axis(1, at=xaxis)
+
+
+hist(CO2PerDiff, breaks=breaks, col=add.alpha(colors[1], alpha), main='', xlab='', ylab='', xlim=xlim, xaxt='n', las=1)
+box(which='plot')
+axis(1, at=xaxis, labels=NA)
+mtext('Flux', 3, 0)
+
+hist(CH4PerDiff, breaks=breaks, col=add.alpha(colors[2], alpha), main='', xlab='', ylab='', xlim=xlim, xaxt='n', las=1)
+box(which='plot')
+axis(1, at=xaxis)
+
+
+mtext('Frequency', 2, .5, outer=T)
+mtext('Percent difference (%)', 1, 1.25, outer=T)
+
+
+dev.off()
+
+
+#Conc
+CO2ConcLakeCum<-cumsum(LakeFlux$CO2_Conc_Mean)
+CO2ConcBuoyCum<-cumsum(BuoyFlux$CO2Buoy_Conc_Mean)
+CH4ConcLakeCum<-cumsum(LakeFlux$CH4_Conc_Mean)
+CH4ConcBuoyCum<-cumsum(BuoyFlux$CH4Buoy_Conc_Mean)
+
+#Flux
+CO2FluxLakeCum<-cumsum(LakeFlux$CO2_Flux_Mean)
+CO2FluxBuoyCum<-cumsum(BuoyFlux$CO2Buoy_Flux_Mean)
+CH4FluxLakeCum<-cumsum(LakeFlux$CH4_Flux_Mean)
+CH4FluxBuoyCum<-cumsum(BuoyFlux$CH4Buoy_Flux_Mean)
+
+#Plot Cumulative Differneces
+plot((CH4FluxBuoyCum-CH4FluxLakeCum)/CH4FluxLakeCum)
+plot((CH4ConcBuoyCum-CH4ConcLakeCum)/CH4ConcLakeCum)
+plot((CO2FluxBuoyCum-CO2FluxLakeCum)/CO2FluxLakeCum)
+plot((CO2ConcBuoyCum-CO2ConcLakeCum)/CO2ConcLakeCum)
+
+
+#Histrograms
+png('Figures/HistogramsCO2CHConc.png', width=8, height=7, units='in', res=200, bg='white')
+par(pch=16)
+par(ps=12)
+par(mfcol=c(2,2))
+par(mar = c(2,1.5,1.5,0.5),mgp=c(2.5,0.4,0),tck=-0.02)
+par(oma=c(1,1.5,0,0))
+par(lend=2)
+par(lwd=2)
+lty=c(1,2)
+pt.cex=1.5
+alpha=0.6
+buoycol='yellow'
+# colors<-c('darkblue', 'darkred')
+
+hist((ConcArray[,,c('CO2uM_t')]), col=add.alpha(colors[1], alpha), freq=F, breaks=seq(0,190,2), xlim=c(5,70), yaxs='i', ylim=c(0,0.115), main='Concentration', ylab='')
+hist(BuoyFlux$CO2Buoy_Conc_Mean, add=T, col=add.alpha(buoycol, alpha), freq=F, breaks=seq(0,190,2), ylab='')
+box(which='plot')
+mtext(expression(paste(CO[2], ' (', mu, 'M)')), 1,2)
+
+legend('topright', inset=0.01, c(expression(CO[2]), expression(CH[4]), 'Buoy'), bty='n', pch=22, pt.cex=3, pt.bg=c(add.alpha(c(colors[1:2], buoycol), alpha)), y.intersp=2)
+
+# hist(log10(ConcArray[,,c('CH4uM_t')]), col=add.alpha(colors[2], alpha), breaks=seq(-3,1,.1), freq=F, ylim=c(0,1.9), xlim=c(-2.2,0.8), yaxs='i', main='', ylab='')
+# hist(log10(BuoyFlux$CH4Buoy_Conc_Mean), add=T, col=add.alpha('yellow', alpha), freq=F, breaks=seq(-3,1,.1), ylab='')
+# box(which='plot')
+# mtext(expression(paste(log[10], ' ', CH[4], ' (', mu, 'M)')), 1,2)
+
+hist((ConcArray[,,c('CH4uM_t')]), col=add.alpha(colors[2], alpha), freq=F,  yaxs='i', main='', ylab='', breaks=seq(0,6,.05), xlim=c(0,2), ylim=c(0,4.5))
+hist((BuoyFlux$CH4Buoy_Conc_Mean), add=T, col=add.alpha(buoycol, alpha), freq=F, breaks=seq(0,6,.05), ylab='')
+box(which='plot')
+mtext(expression(paste(CH[4], ' (', mu, 'M)')), 1,2)
+
+
+hist((fluxmatrix[,,'CO2']), col=add.alpha(colors[1], alpha), freq=F,  yaxs='i', main='Flux', breaks=seq(-100,800,10), xlim=c(-50,300), ylim=c(0,0.0349), ylab='')
+hist(BuoyFlux$CO2Buoy_Flux_Mean, add=T, col=add.alpha(buoycol, alpha), freq=F, breaks=seq(-100,800,10), ylab='')
+box(which='plot')
+mtext(expression(paste(CO[2], ' efflux (mmol m'^'-2', ' d'^'-1', ')')), 1,2)
+# 
+# hist(log10(fluxmatrix[,,'CH4']), col=add.alpha(colors[2], alpha), freq=F,  yaxs='i', main='', breaks=seq(-4,2,.15), xlim=c(-2,1.5), ylim=c(0,1.199), ylab='')
+# hist(log10(BuoyFlux$CH4Buoy_Flux_Mean), add=T, col=add.alpha('yellow', alpha), freq=F, breaks=seq(-4,2,.15), ylab='')
+# box(which='plot')
+# mtext(expression(paste(log[10], ' ', CH[4], ' efflux (mmol m'^'-2', ' d'^'-1', ')')), 1,2)
+
+hist((fluxmatrix[,,'CH4']), col=add.alpha(colors[2], alpha), freq=F,  yaxs='i', main='',  ylab='', breaks=seq(-.5, 25, .15), ylim=c(0,1.3), xlim=c(0,6))
+hist(BuoyFlux$CH4Buoy_Flux_Mean, add=T, col=add.alpha(buoycol, alpha), freq=F, breaks=seq(-.5, 25, .15), ylab='')
+box(which='plot')
+mtext(expression(paste(CH[4], ' efflux (mmol m'^'-2', ' d'^'-1', ')')), 1,2)
+
+
+mtext('Density', 2,0.25, outer=T)
+
+dev.off()
 
