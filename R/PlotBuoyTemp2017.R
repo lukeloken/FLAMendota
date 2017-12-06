6d2a743bcdbf3b4175eddbc6dd2d62219c2018c8
lukeloken <lukeloken@gmail.com>
2017-12-06 10:49:52 -0600

    Working to get all of 2017 temperature plot

--- R/PlotBuoyTemp2017.R
+++ R/PlotBuoyTemp2017.R
@@ -9,14 +9,89 @@ library(stringr)
 library(RcppRoll)
 library(lubridate)
 library(viridis)
+library(akima)
 
 mindate<-as.POSIXct('2017-09-07 00:00:00')
 cleandate<-as.POSIXct('2017-09-25 15:40:00', tz='America/Chicago')
 colors<-c('darkred', 'darkblue', 'olivedrab')
 
+#Data for all of 2017
+LTERdf_all<-read.csv('Data/sensor_mendota_lake_watertemp_hourly_All2017.csv', header=T, stringsAsFactors = F)
+LTERdf_all$sample_time<-str_pad(LTERdf_all$hour, 4, pad = "0")
+LTERdf_all$datetime<-as.POSIXct(paste(LTERdf_all$sampledate, LTERdf_all$sample_time, sep=" "), format='%Y-%m-%d %H%M')
 
-# LTERdf<-read.csv('Data/sensor_mendota_lake_watertemp_hi_res_Sep2017.csv', header=T, stringsAsFactors = F)
 
+#omits NAs and INF
+LTERdf_sub<-LTERdf_all[is.finite(LTERdf_all$wtemp),]
+
+#Construct interpolated surface to plot
+dates1=LTERdf_sub$datetime[1:45011]
+depths1=LTERdf_sub$depth[1:45011]
+value1=LTERdf_sub$wtemp[1:45011]
+interp_dates1<-seq(ceiling_date(min(dates1), "hours"),floor_date(max(dates1), "hours"), by='hours')
+interp1 <- akima::interp(dates1,depths1,value1, yo=seq(min(depths1),max(depths1), by=1), xo=interp_dates1, extrap=F)
+names(interp1)<-c('Date', 'Depth', 'wtemp')
+interp1$DateTime<-(interp_dates1)
+
+#Second half
+dates2=LTERdf_sub$datetime[45012:length(LTERdf_sub$datetime)]
+depths2=LTERdf_sub$depth[45012:length(LTERdf_sub$depth)]
+value2=LTERdf_sub$wtemp[45012:length(LTERdf_sub$wtemp)]
+interp_dates2<-seq(ceiling_date(min(dates2), "hours"),floor_date(max(dates2), "hours"), by='hours')
+interp2 <- akima::interp(dates2,depths2,value2, yo=seq(min(depths2),max(depths2), by=1), xo=interp_dates2, extrap=F)
+names(interp2)<-c('Date', 'Depth', 'wtemp')
+interp2$DateTime<-(interp_dates2)
+
+interp<-list()
+interp$DateTime<-c(interp1$DateTime, interp2$DateTime)
+interp$Depth <-interp1$Depth 
+interp$wtemp<-rbind(interp1$wtemp, interp2$wtemp)
+
+
+#Plot 2017 Temp Data
+# png('Figures/TempBathyGram_All2017.png', width=7, height=3, units='in', res=400, bg='white')
+par(pch=16)
+par(ps=10)
+par(mfrow=c(1,1))
+par(mar = c(3,3.5,0.5,0.5),mgp=c(1.5,0.4,0),tck=-0.02)
+par(lend=2)
+
+# datelabels<-unique(year(dates))
+# datelocation<- (365.25*(datelabels-1970))
+
+filled.contour(x=interp[[1]], y=interp[[2]], z=interp[[3]], ylim=c(max(interp[[2]]), 0), nlevels = 40, color.palette = colorRampPalette(c(viridis(6, begin=.2, end=.98), rev(magma(5, begin=.35, end=.98))), bias=1), ylab="Depth (m)")
+
+mtext(expression(paste("Water temperature (", degree, "C)", sep="")), 4, -6)
+
+# dev.off()
+
+
+
+#Construct interpolated surface to plot
+dates=LTERdf_sub$datetime
+depths=LTERdf_sub$depth
+value=LTERdf_sub$wtemp
+interp_dates<-seq(ceiling_date(min(dates), "days"),floor_date(max(dates), "days"), by='days')
+interp_depths<-seq(min(depths),max(depths), by=.5)
+interp <- akima::interp(dates,depths,value, yo=interp_depths, xo=interp_dates, linear=F)
+names(interp)<-c('Date', 'Depth', 'wtemp')
+interp$DateTime<-(interp_dates)
+
+par(pch=16)
+par(ps=10)
+par(mfrow=c(1,1))
+par(mar = c(3,3.5,0.5,0.5),mgp=c(1.5,0.4,0),tck=-0.02)
+par(lend=2)
+
+
+filled.contour(x=interp[[4]], y=interp[[2]], z=interp[[3]], ylim=c(max(interp[[2]]), 0), nlevels = 40, color.palette = colorRampPalette(c(viridis(6, begin=.2, end=.98), rev(magma(5, begin=.35, end=.98))), bias=1), ylab="Depth (m)")
+
+
+
+###################
+
+
+#High resolution data for Fall 2017
 LTERdf<-read.csv('Data/sensor_mendota_lake_watertemp_hi_res_Oct4_2017.csv', header=T, stringsAsFactors = F)
 LTERdf$sample_time<-str_pad(LTERdf$sample_time, 4, pad = "0")
 
@@ -27,11 +102,16 @@ LTERdf2$sample_time<-str_pad(LTERdf2$sample_time, 4, pad = "0")
 
 LTERdf2$datetime<-as.POSIXct(paste(LTERdf2$sampledate, LTERdf2$sample_time, sep=" "), format='%Y-%m-%d %H%M')
 
-LTERdf3<-smartbind(LTERdf, LTERdf2)
-LTERdf3$datetime<-as.POSIXct(LTERdf3$datetime, format='%Y-%m-%d %H:%M:%S')
-LTERdf4 <- unique( LTERdf3[ , ] )
+LTERdf3<-read.csv('Data/sensor_mendota_lake_watertemp_hi_res_Nov30_2017.csv', header=T, stringsAsFactors = F)
+LTERdf3$sample_time<-str_pad(LTERdf3$sample_time, 4, pad = "0")
 
-LTERdf_subset<-LTERdf4[LTERdf4$datetime>mindate,]
+LTERdf3$datetime<-as.POSIXct(paste(LTERdf3$sampledate, LTERdf3$sample_time, sep=" "), format='%Y-%m-%d %H%M')
+
+LTERdf4<-smartbind(LTERdf, LTERdf2, LTERdf3)
+LTERdf4$datetime<-as.POSIXct(LTERdf4$datetime, format='%Y-%m-%d %H:%M:%S')
+LTERdf5 <- unique( LTERdf4[ , ] )
+
+LTERdf_subset<-LTERdf5[LTERdf5$datetime>mindate,]
 
 LTERdf_subset<-LTERdf_subset[order(LTERdf_subset$datetime),]
 
@@ -75,7 +155,7 @@ t.d.t.roll<-roll_mean(t.d.t, n=60*2+1, align='center', fill=NA)
 # lines(wrt_top[,1], t.d.t.roll, lwd=1, col='magenta')
 
 
-png('Figures/TempBathyGram2017.png', width=7.15, height=3.5, units='in', res=400, bg='white')
+png('Figures/TempBathyGramSepNov2017.png', width=7.15, height=3.5, units='in', res=400, bg='white')
 par(pch=16)
 par(ps=10)
 par(mfrow=c(1,1))
@@ -90,7 +170,7 @@ mtext(expression(paste("Water temperature (", degree, "C)", sep="")), 4, -6)
 
 dev.off()
 
-png('Figures/TempBathyGram2017_WithThermocline.png', width=7.15, height=3.5, units='in', res=400, bg='white')
+png('Figures/TempBathyGramSepNov2017_WithThermocline.png', width=7.15, height=3.5, units='in', res=400, bg='white')
 par(pch=16)
 par(ps=10)
 par(mfrow=c(1,1))
@@ -109,7 +189,7 @@ mtext(expression(paste("Water temperature (", degree, "C)", sep="")), 4, -6)
 dev.off()
 
 
-png('Figures/TempBathyGram2017_WithThermoclineViridis.png', width=7.15, height=3.5, units='in', res=400, bg='white')
+png('Figures/TempBathyGramSepNov2017_WithThermoclineViridis.png', width=7.15, height=3.5, units='in', res=400, bg='white')
 par(pch=16)
 par(ps=10)
 par(mfrow=c(1,1))
@@ -128,7 +208,27 @@ mtext(expression(paste("Water temperature (", degree, "C)", sep="")), 4, -6)
 dev.off()
 
 
-LTERmet<-read.csv('Data/sensor_mendota_lake_met_hourly_Oct26_2017.csv', header=T, stringsAsFactors = F)
+
+png('Figures/TempBathyGramSepNov2017_Viridis.png', width=7.5, height=3, units='in', res=400, bg='white')
+par(pch=16)
+par(ps=10)
+par(mfrow=c(1,1))
+par(mar = c(3,3.5,0.5,0.5),mgp=c(1.5,0.4,0),tck=-0.02)
+par(lend=2)
+
+
+xticks<-seq(ceiling_date(min(dates), "weeks"),floor_date(max(dates), "weeks"), by='weeks')
+xlabels<-paste(lubridate::month(xticks, label=TRUE, abbr=T), day(xticks), sep=" ")
+
+
+filled.contour(x=dates, y=depths, z=wrt, ylim=c(max(depths), 0), nlevels = 40, color.palette = colorRampPalette(c(viridis(6, begin=.2, end=.98), rev(magma(5, begin=.35, end=.98))), bias=1), ylab="Depth (m)", plot.axes = { axis(1, at=xticks, labels=xlabels); axis(1, at=xticks[2], labels=xlabels[2]); axis(2)})
+
+mtext(expression(paste("Water temperature (", degree, "C)", sep="")), 4, -6)
+
+dev.off()
+
+
+LTERmet<-read.csv('Data/sensor_mendota_lake_met_hourly_All2017.csv', header=T, stringsAsFactors = F)
 LTERmet$sample_time<-str_pad(LTERmet$hour, 4, pad = "0")
 
 LTERmet$datetime<-as.POSIXct(paste(LTERmet$sampledate, LTERmet$sample_time, sep=" "), format='%Y-%m-%d %H%M')
