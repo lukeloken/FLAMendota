cc75e5b9bee53958880aa5e94112bafd474532fe
lukeloken <lukeloken@gmail.com>
2017-12-08 13:53:37 -0600

    Add uM plots for CH4 timeseries

--- R/PlotCarbon2016.R
+++ R/PlotCarbon2016.R
@@ -275,6 +275,73 @@ box(which='plot')
 
 dev.off()
 
+
+# Methane uM boxplots
+# Code plots a polygon of IQR across entire lake surface, mean, median, and buoy
+
+png('Figures/Methane2016uM.png', width=8, height=4, units='in', res=200, bg='white')
+par(pch=16)
+par(ps=12)
+par(mfrow=c(1,1))
+par(mar = c(3,3.5,0.5,0.5),mgp=c(1.5,0.4,0),tck=-0.02)
+par(lend=2)
+
+xticks<-seq(ceiling_date(min(LGRList$Mean$Date), "months"),floor_date(max(LGRList$Mean$Date), "months"), by='months')
+xlabels<-paste(month(xticks, label=TRUE, abbr=T), " 1", sep="")
+buoypch<-20
+
+CH4_ylim<-range(c(LGRList$Mean$CH4uM_t, LGRList$Q05$CH4uM_t, LGRList$Q95$CH4uM_t, Buoy_daily$CH4uM), na.rm=T)
+# co2_ylim[2]<-150
+
+plot(LGRList$Mean$Date, LGRList$Mean$CH4uM_t, type="n", pch=15, ylim=CH4_ylim, ylab="", xlab="", xaxt="n")
+axis(1, at=xticks, labels=xlabels)
+
+mtext(expression(paste(CH[4], " (", mu, "M)", sep="")), 2, 2)
+mtext('2016', 1, 1.5)
+
+CH4eq<-gas.at.sat(LGRList$Mean$TempC, gas='CH4')
+lines(LGRList$Mean$Date, CH4eq, type="l", lty=3)
+
+# Polygon of 5-95%
+polyx90<-c(LGRList$Q05$Date, rev(LGRList$Q95$Date))
+polyy90<-c(LGRList$Q05$CH4uM_t, rev(LGRList$Q95$CH4uM_t))
+polygon(polyx90, polyy90, border=colors[3], col=colors[3])
+
+# Polygon of IQR
+polyx<-c(LGRList$Q25$Date, rev(LGRList$Q75$Date))
+polyy<-c(LGRList$Q25$CH4uM_t, rev(LGRList$Q75$CH4uM_t))
+polygon(polyx, polyy, border=colors[5], col=colors[5])
+
+# Mean and Median
+points(LGRList$Median$Date, LGRList$Median$CH4uM_t, type="l", col=colors[2], lwd=2)
+# points(LGRList$Mean$Date, LGRList$Mean$CH4St_t/100, type="l", col=colors[1], lwd=2)
+
+#Buoy
+points(Buoy_daily$Date, Buoy_daily$CH4uM_t, type="o", col=colors[4], pch=buoypch, cex=1.5, lty=2, lwd=1.5)
+
+usr<-par('usr')
+yscale<-diff(usr[3:4])/10
+box.y<-usr[4]-yscale*seq(0.5,4, length.out=7)
+
+xscale<-diff(usr[1:2])/30
+box.x<-usr[1]+xscale*seq(0.7,2, length.out=2)
+
+polygon(x=c(box.x, rev(box.x)), y=c(rep(box.y[5],2), rep(box.y[1],2)), col=colors[3], border=colors[3], lwd=2)
+polygon(x=c(box.x, rev(box.x)), y=c(rep(box.y[4],2), rep(box.y[2],2)), col=colors[5], border=colors[5], lwd=2)
+polygon(x=c(box.x, rev(box.x)), y=c(rep(box.y[3],2), rep(box.y[3],2)), col=colors[2], border=colors[2], lwd=2)
+
+lines(x=box.x, y=rep(box.y[6], 2), lty=2, col=colors[4], type="l", pch=16, lwd=1.5)
+lines(x=box.x, y=rep(box.y[7], 2), lty=3, type="l")
+points(x=mean(box.x), y=box.y[6], col=colors[4], type="p", pch=buoypch, cex=1.5)
+
+text(x=box.x[2], y=box.y, c(expression(paste(Q[95])),  expression(paste(Q[75])), expression(paste(Q[50])), expression(paste(Q[25])), expression(paste(Q[5])), 'Buoy', "Atm"), pos=4)
+
+box(which='plot')
+
+dev.off()
+
+
+
 #Plot CO2 and CH4 concentration through time
 
 png('Figures/CarbonDioxideMethaneConc2016.png', width=6, height=5, units='in', res=400, bg='white')
